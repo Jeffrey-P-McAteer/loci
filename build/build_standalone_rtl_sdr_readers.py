ac5a2b5519c76cbd652adc6d1a2f95675ab3cd6c
Jeffrey McAteer <jeffrey.mcateer@devil-tech.com>
2020-12-22 13:35:52 -0500

    windows zadic.exe updates

--- build/build_standalone_rtl_sdr_readers.py
+++ build/build_standalone_rtl_sdr_readers.py
@@ -90,13 +90,13 @@ def build(eapp_dir):
         [
           'C:\\tools\\cygwin\\bin\\bash.exe',
           '-lc', '''
-            cd {cwd} ;
+            cd "{cwd}" ;
             export PATH="C:\\\\tools\\\\cygwin\\\\bin:{libtool_bin_d}\\\\bin:$PATH"  ;
             ./bootstrap.sh ;
             ./configure --enable-static --enable-64bit --enable-examples-build --with-libusb0="{libusb_d}" ;
             make -j4
           '''.format(
-            cwd=os.path.abspath(libwidi_d),
+            cwd=os.path.abspath(libwidi_d).replace('\\', '\\\\'),
             libusb_d=libusb_d,
             libtool_bin_d=libtool_bin_d,
           )
