69ad9cb9748ac3c7f37a658b227ed495315cd1ce
Jeffrey McAteer <jeffrey.mcateer@devil-tech.com>
2020-12-23 12:38:53 -0500

    nonsense

--- plugins/win64_libusb_installer.py
+++ plugins/win64_libusb_installer.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 
     # Run through values of DriverEdit2 until it contains 'libusb-win32'
     max_tries = 50
-    while not ( 'libusb-win32' in winspec['DriverEdit2'].get_value() ) and max_tries > 0:
+    while not ( 'libusb-win32' in winspec['Zadig']['DriverEdit2'].get_value() ) and max_tries > 0:
       max_tries -= 1
       winspec['UpDown'].increment()
 
