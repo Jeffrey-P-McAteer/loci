3c56ffb1874beb4308187e4ec974e4f36f3e05c1
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2021-08-07 11:39:03 -0400

    ci task experiments

---
+++ misc-res/loci-web-deploy.py
@@ -0,0 +1,14 @@
+# Run by loci-web-deploy.service
+# after loci repo has been cloned
+
+import os
+import sys
+import subprocess
+
+
+def main(args=sys.argv):
+  print('TODO build + call modified webpage_update_tool targeting /var/www or similar')
+  
+
+if __name__ == '__main__':
+  main()
